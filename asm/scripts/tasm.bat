cc182c0db214c2339f6d96de171ead0a7935ae30
Aaronjamt <>
2020-08-22 17:37:35 -0700

    Oops, screwed up TASM table path

--- asm/scripts/tasm.bat
+++ asm/scripts/tasm.bat
@@ -1,10 +1,9 @@
 @echo off
 
-set TASMTABS=%CD%
-
 cd ..
 rmdir /S /Q build
 mkdir build
 copy src\pongiii.asm build\pongiii.asm
 
+set TASMTABS=%CD%\tasm
 tasm\tasm -80 -i -b build\pongiii.asm build\pongiii.bin
