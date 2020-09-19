2ee4a9dc7b18ac998ce3847b7d553622fbf3c419
Aaronjamt <Aaronjamt>
2020-09-18 17:02:48 -0700

    Fix tasm.bat to compile correctly

--- TI-86/scripts/tasm.bat
+++ TI-86/scripts/tasm.bat
@@ -6,4 +6,5 @@ mkdir build >nul 2>nul
 copy src\pongiii.asm build\pongiii.asm >nul 2>nul
 
 set TASMTABS=%CD%\tasm
-tasm\tasm -80 -i -b build\pongiii.asm build\pongiii.bin
+cd tasm
+tasm -80 -i -b ..\build\pongiii.asm ..\build\pongiii.bin ..\build\pongiii.lst
