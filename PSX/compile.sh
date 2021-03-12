7dbee4e3d8a2286c09de0629bce0f73dd3bf6e05
Aaronjamt <aaronjamt@gmail.com>
2021-03-11 19:02:15 -0800

    Update compile.sh

--- PSX/compile.sh
+++ PSX/compile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+# Set CWD to the script's parent dir (in case we are called from another dir)
 cd $( dirname "${BASH_SOURCE[0]}" )
+# Setup env so we can access the SDK
 source SDK/env.source
 
-rm -rf out >/dev/null 2>&1
-mkdir out
-
+# Clean up old files (otherwise changes may not be applied) and build!
 cd src
-remake -j4
+remake -j4 clean all
