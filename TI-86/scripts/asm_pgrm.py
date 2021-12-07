03bf1790af90e05ef1f8ccc54b47037cca1eb7a3
Aaronjamt <13600347+aaronjamt@users.noreply.github.com>
2021-12-07 11:39:16 -0800

    Update asm_pgrm.py

--- TI-86/scripts/asm_pgrm.py
+++ TI-86/scripts/asm_pgrm.py
@@ -1,4 +1,4 @@
-import socket, time
+import os, socket, time
 
 # b'\x28\x00', b'\x3A\x00', b'\x34\x00', b'\x11\x00', b'\x83\x00', b'\xC2\x00', b'\xC2\x00'
 #     ASM({PRGM}{F1}{F1}
@@ -9,7 +9,7 @@ import socket, time
 keys = [b'\x28\x00', b'\x3A\x00', b'\x34\x00', b'\x11\x00', b'\x83\x00', b'\xC2\x00', b'\xC2\x00', b'\x07\x00', b'\x07\x00', b'\x06\x00']
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(("10.6.61.134", 86))
+sock.connect((os.getenv("TI_ADDR"), 86))
 
 for key in keys:
     sock.sendall(b'\x06\x87'+key)
